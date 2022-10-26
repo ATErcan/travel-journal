10bcb73066de1fe773b5edea5131bdcb7248a15e
Talha <atercan0658@gmail.com>
2022-10-26 21:05:32 +0200

    data.js updated

--- src/helper/data.js
+++ src/helper/data.js
@@ -32,4 +32,52 @@ export const data = [
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
     imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
   },
+  {
+    id: 4,
+    title: "Eiffel Tower",
+    location: "France",
+    googleMapsUrl: "https://goo.gl/maps/8mykK9NGp93bkiN39",
+    startDate: "21 Nov, 2021",
+    endDate: "23 Nov, 2021",
+    description:
+      "The symbol of Paris and one of the most photographed structures in the world, a visit to the Eiffel Tower is a must for all travelers. Few landmarks inspire such a passion for travel as this single iron structure.",
+    imageUrl:
+      "https://img.freepik.com/free-photo/famous-eiffel-tower-paris-with-gorgeous-colors_268835-830.jpg?w=1380&t=st=1666810318~exp=1666810918~hmac=d1f1b6e9f6adfb76be70ed77dbdb2814688b7820c5247df9497c78d245e6b4c7",
+  },
+  {
+    id: 5,
+    title: "Machu Picchu",
+    location: "Peru",
+    googleMapsUrl: "https://goo.gl/maps/uRgz2GA6S8YgT5S38",
+    startDate: "30 Nov, 2021",
+    endDate: "07 Dec, 2021",
+    description:
+      "The ancient Inca city of Machu Picchu is arguably the most impressive ruined city in the world. Set on a high plateau with soaring green mountains, the setting is surreal. The sheer tenacity of the original builders to create this amazing place in what would have been impenetrable jungle, is, in itself, impressive.",
+    imageUrl:
+      "https://img.freepik.com/free-photo/bird-s-eye-view-breathtaking-mountain-machu-picchu-peru_181624-10826.jpg?w=1380&t=st=1666810430~exp=1666811030~hmac=08eee83cca14c5369f33cafac97db4148e7d976e65c1e9f3f350755ea5040e48",
+  },
+  {
+    id: 6,
+    title: "Pyramids of Giza",
+    location: "Egypt",
+    googleMapsUrl: "https://goo.gl/maps/B6gjDQALemUuUm2F9",
+    startDate: "15 Dec, 2021",
+    endDate: "21 Dec, 2021",
+    description:
+      "Pyramids of Giza were built over 4,500 years ago. Located just outside Cairo, the pyramids, which is also where you'll find the Sphinx, are easy to get to, and tours are easy to arrange. A sunset camel ride around the structures is a wonderful experience.",
+    imageUrl:
+      "https://img.freepik.com/free-photo/cairo-egypt-october-2020-camel-sitting-pyramid-khafren-with-man-pyramids-giza-oldest-funerary-monument-world_242111-17421.jpg?w=1380&t=st=1666810753~exp=1666811353~hmac=3ac1327d7310bc5823751aa95e32f6a21d17b584ccce7e7964a32711a9e018f2",
+  },
+  {
+    id: 7,
+    title: "Grand Canyon",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/ur2Pw1sFarzxJvpz7",
+    startDate: "15 May, 2022",
+    endDate: "22 May, 2022",
+    description:
+      "The greatest natural attraction in the United States, the Grand Canyon is a key sight for all travelers planning their lifetime of adventures. Standing on the rim of the Grand Canyon, looking out over the carved landscape, will awaken your senses.",
+    imageUrl:
+      "https://img.freepik.com/free-photo/grand-canyon-landscape-sunny-day_26085-14.jpg?w=1380&t=st=1666811002~exp=1666811602~hmac=228ccba0f944e6a2eba534fbe0c1a3cea2bd6cb9a8e45d87d2cea77351a84ef8",
+  },
 ];
