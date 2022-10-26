34cd0f2192e2d79ce5e61d531a632cf8d443f05b
Talha <atercan0658@gmail.com>
2022-10-26 18:57:59 +0200

    footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/ATErcan">-Credits-</a>
+      <p>Me - A full-stack developer of the near future...</p>
+    </footer>
+  );
+};
+
+export default Footer;
